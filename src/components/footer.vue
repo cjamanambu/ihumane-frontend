27ab3ebbdc45beb078280815cf30607c828b1ce5
CJ Amanambu <oamanambu@yahoo.com>
2021-12-07 15:57:59 +0100

    initial commit

---
+++ src/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6">2021 © IHUMANE</div>
+        <div class="col-sm-6">
+          <div class="text-sm-right d-none d-sm-block">
+            Powered by
+            <a href="https://telecom.connexxiongroup.com">Connexxion Telecom</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/menu.js
@@ -0,0 +1,17 @@
+export const menuItems = [
+  {
+    id: 1,
+    label: "Administrative Menu",
+    isTitle: true,
+  },
+  {
+    id: 2,
+    label: "Dashboard",
+    icon: "ri-dashboard-line",
+    badge: {
+      variant: "success",
+      text: "menuitems.dashboard.badge",
+    },
+    link: "/",
+  },
+];
