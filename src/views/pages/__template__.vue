c969a896130c3bd763640853b1648e37fd045707
CJ Amanambu <oamanambu@yahoo.com>
2021-12-17 17:38:14 +0100

    finished state

--- src/views/pages/__template__.vue
+++ src/views/pages/__template__.vue
@@ -13,7 +13,6 @@ export default {
   },
   data() {
     return {
-      submitting: false,
       title: "Banks",
       items: [
         {
--- src/views/pages/hr-configuration/states.vue
+++ src/views/pages/hr-configuration/states.vue
@@ -2,6 +2,8 @@
 import Layout from "@/views/layouts/main";
 import PageHeader from "@/components/page-header";
 import appConfig from "@/app.config";
+import { required } from "vuelidate/lib/validators";
+
 export default {
   page: {
     title: "States",
@@ -11,9 +13,74 @@ export default {
     Layout,
     PageHeader,
   },
+  mounted() {
+    this.refreshTable();
+  },
+  validations: {
+    name: { required },
+  },
+  methods: {
+    refreshTable() {
+      this.apiGet(this.ROUTES.state, "Get States Error").then((res) => {
+        const { data } = res;
+        this.states = data;
+        this.totalRows = this.states.length;
+      });
+    },
+    submitNew() {
+      this.submitted = true;
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.apiFormHandler("Invalid State");
+      } else {
+        const data = {
+          state_name: this.name,
+        };
+        this.apiPost(this.ROUTES.state, data, "Add State Error").then((res) => {
+          this.apiResponseHandler(`${res.data}`, "New State Added");
+          this.refreshTable();
+          this.$v.$reset();
+          this.$refs["add-state"].hide();
+        });
+      }
+    },
+    submitUpdate() {
+      this.submitted = true;
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.apiFormHandler("Invalid State");
+      } else {
+        const data = {
+          state_name: this.name,
+        };
+        const url = `${this.ROUTES.state}/${this.stateID}`;
+        this.apiPatch(url, data, "Update State Error").then((res) => {
+          this.apiResponseHandler(`${res.data}`, "Update Successful");
+          this.refreshTable();
+          this.$v.$reset();
+          this.$refs["update-state"].hide();
+        });
+      }
+    },
+    resetForm() {
+      this.name = null;
+      this.$v.$reset();
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+    selectState(state) {
+      state = state[0];
+      this.name = state.s_name;
+      this.stateID = state.s_id;
+      this.$refs["update-state"].show();
+      this.$refs["state-table"].clearSelected();
+    },
+  },
   data() {
     return {
-      submitting: false,
       title: "States",
       items: [
         {
@@ -28,6 +95,22 @@ export default {
           active: true,
         },
       ],
+      submitted: false,
+      states: [],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 10,
+      pageOptions: [10, 25, 50, 100],
+      filter: null,
+      filterOn: [],
+      sortBy: "s_id",
+      sortDesc: false,
+      fields: [
+        { key: "s_id", label: "ID", sortable: true },
+        { key: "s_name", label: "State Name", sortable: true },
+      ],
+      name: null,
+      stateID: null,
     };
   },
 };
@@ -42,5 +125,168 @@ export default {
         Add State
       </b-button>
     </div>
+    <div v-if="this.apiBusy" style="margin-left: 50%; margin-top: 20%">
+      <b-spinner type="grow" class="m-2" variant="success" />
+    </div>
+    <div v-else class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="row mt-4">
+              <div class="col-sm-12 col-md-6">
+                <div id="tickets-table_length" class="dataTables_length">
+                  <label class="d-inline-flex align-items-center">
+                    Show&nbsp;
+                    <b-form-select
+                      v-model="perPage"
+                      size="sm"
+                      :options="pageOptions"
+                    ></b-form-select
+                    >&nbsp;entries
+                  </label>
+                </div>
+              </div>
+              <!-- Search -->
+              <div class="col-sm-12 col-md-6">
+                <div
+                  id="tickets-table_filter"
+                  class="dataTables_filter text-md-right"
+                >
+                  <label class="d-inline-flex align-items-center">
+                    Search:
+                    <b-form-input
+                      v-model="filter"
+                      type="search"
+                      placeholder="Search..."
+                      class="form-control form-control-sm ml-2"
+                    ></b-form-input>
+                  </label>
+                </div>
+              </div>
+              <!-- End search -->
+            </div>
+            <!-- Table -->
+            <div class="table-responsive mb-0">
+              <b-table
+                ref="state-table"
+                bordered
+                selectable
+                hover
+                :items="states"
+                :fields="fields"
+                responsive="sm"
+                :per-page="perPage"
+                :current-page="currentPage"
+                :sort-by.sync="sortBy"
+                :sort-desc.sync="sortDesc"
+                :filter="filter"
+                :filter-included-fields="filterOn"
+                @filtered="onFiltered"
+                show-empty
+                select-mode="single"
+                @row-selected="selectState"
+              >
+              </b-table>
+            </div>
+            <div class="row">
+              <div class="col">
+                <div
+                  class="dataTables_paginate paging_simple_numbers float-right"
+                >
+                  <ul class="pagination pagination-rounded mb-0">
+                    <!-- pagination -->
+                    <b-pagination
+                      v-model="currentPage"
+                      :total-rows="totalRows"
+                      :per-page="perPage"
+                    ></b-pagination>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <b-modal
+      ref="add-state"
+      title="Add State"
+      hide-footer
+      centered
+      title-class="font-18"
+      @hidden="resetForm"
+    >
+      <form @submit.prevent="submitNew">
+        <div class="form-group">
+          <label for="name">
+            State Name <span class="text-danger">*</span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            v-model="name"
+            class="form-control"
+            :class="{
+              'is-invalid': submitted && $v.name.$error,
+            }"
+          />
+        </div>
+        <b-button
+          v-if="!submitting"
+          class="btn btn-success btn-block mt-4"
+          type="submit"
+        >
+          Submit
+        </b-button>
+        <b-button
+          v-else
+          disabled
+          class="btn btn-success btn-block mt-4"
+          type="submit"
+        >
+          Submitting...
+        </b-button>
+      </form>
+    </b-modal>
+    <b-modal
+      ref="update-state"
+      title="Update State"
+      hide-footer
+      centered
+      title-class="font-18"
+      @hidden="resetForm"
+    >
+      <form @submit.prevent="submitUpdate">
+        <div class="form-group">
+          <label for="name">
+            State Name <span class="text-danger">*</span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            v-model="name"
+            class="form-control"
+            :class="{
+              'is-invalid': submitted && $v.name.$error,
+            }"
+          />
+        </div>
+        <b-button
+          v-if="!submitting"
+          class="btn btn-success btn-block mt-4"
+          type="submit"
+        >
+          Submit
+        </b-button>
+        <b-button
+          v-else
+          disabled
+          class="btn btn-success btn-block mt-4"
+          type="submit"
+        >
+          Submitting...
+        </b-button>
+      </form>
+    </b-modal>
   </Layout>
 </template>
