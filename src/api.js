b3a4b637729da71ec87856cc78680e0892735705
Joseph Gbudu Joseph <josephgbudujoseph@Josephs-MacBook-Pro.local>
2022-03-24 17:12:56 +0100

    baseURL changed

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  //baseURL: `https://irc-api.ihumane.net/`,
-  baseURL: `http://localhost:9829/`,
+  baseURL: `https://irc-api.ihumane.net/`,
+  //baseURL: `http://localhost:9829/`,
 });
 
 export function SET_TOKEN(token) {
