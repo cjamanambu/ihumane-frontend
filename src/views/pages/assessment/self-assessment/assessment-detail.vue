490c949fc79af07b1b59547d453598a867fca61b
CJ Amanambu <oamanambu@yahoo.com>
2022-05-05 16:10:11 +0100

    finished assessment details for end of year

--- src/views/pages/assessment/self-assessment/assessment-detail.vue
+++ src/views/pages/assessment/self-assessment/assessment-detail.vue
@@ -51,10 +51,10 @@ export default {
           active: true,
         },
       ],
-      authuser:null,
-      goalSettingId:null,
+      authuser: null,
+      goalSettingId: null,
       texts: [{ id: 0, goal: null }],
-      assessStatus:0,
+      assessStatus: 0,
       //supervisor:null,
       year: null,
       endYearQuestions: [],
@@ -79,7 +79,7 @@ export default {
       assessments: [],
       prefillAssessments: [],
       empId: this.$route.params.empid,
-      selectedYear:null,
+      selectedYear: null,
       statuses: [
         { text: "CLOSE", value: 1 },
         { text: "OPEN", value: 0 },
@@ -124,7 +124,6 @@ export default {
         this.gsID = data.openGoal[0].gs_id;
         this.selectedYear = data.openGoal[0].gs_year;
         data.questions.forEach(async (datum) => {
-
           const dat = {
             id: datum.sa_id,
             goal: datum.sa_comment,
@@ -142,7 +141,7 @@ export default {
         //}
       });
     },
-    async getSelfAssessmentMaster(){
+    async getSelfAssessmentMaster() {
       //const urls = `${this.ROUTES.selfAssessment}/get-self-assessment-master/${this.$route.params.empid}/${this.gsID}`;
       /*await this.apiGet(urls).then(async (res) => {
        // const { data } = res;
@@ -231,22 +230,21 @@ export default {
         });
       }
     },
-    processAssessment(){
-
+    processAssessment() {
       const employeeID = this.$route.params.empid;
       const gsId = this.gsID;
       const url = `${this.ROUTES.selfAssessment}/approve-assessment/${employeeID}/${gsId}`;
       const data = {
         gs_id: gsId,
-        emp_id:employeeID,
+        emp_id: employeeID,
         //supervisor:this.getEmployee.emp_id
       };
       this.apiPost(url, data, "Could not process request").then(() => {
-        this.apiResponseHandler("Process Complete", "Employee self-assessment completed.");
-
+        this.apiResponseHandler(
+          "Process Complete",
+          "Employee self-assessment completed."
+        );
       });
-
-
     },
     fetchEmployee() {
       const employeeID = this.$route.params.empid;
@@ -254,7 +252,6 @@ export default {
       this.apiGet(url, "Get Employee Error").then((res) => {
         this.currentEmployee = res.data;
         //console.log('Supervisor: '+this.supervisor);
-
       });
     },
   },
@@ -279,97 +276,102 @@ export default {
     </div>
     <scale-loader v-if="apiBusy" />
     <div v-else>
-
-        <div class="row">
-          <div class="col-lg-8">
-            <div class="card">
-              <div class="card-body">
-                <div class="p-3 bg-light mb-4">
-                  <h5 class="font-size-14 mb-0">Beginning of the Year </h5>
-                </div>
-                <form
-                  v-if="assessments.length > 0"
-                  @submit.prevent="updateGoals"
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="card">
+            <div class="card-body">
+              <div class="p-3 bg-light mb-4">
+                <h5 class="font-size-14 mb-0">Beginning of the Year</h5>
+              </div>
+              <form v-if="assessments.length > 0" @submit.prevent="updateGoals">
+                <div
+                  class="row"
+                  v-for="(field, index) in assessments"
+                  :key="index"
                 >
-                  <div
-                    class="row"
-                    v-for="(field, index) in assessments"
-                    :key="index"
-                  >
-                    <div class="col-lg-12">
-                      <div class="row">
-                        <div class="col-12">
-                          <div class="form-group">
-                            <label for="goal" class="bg-light p-1">
-                              Goal {{ index + 1 }}
-                              <span class="text-danger">*</span>
-                            </label>
-                            <p style="margin-left: 30px!important;" v-html="field.goal"></p>
-                          </div>
+                  <div class="col-lg-12">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label for="goal" class="bg-light p-1">
+                            Goal {{ index + 1 }}
+                            <span class="text-danger">*</span>
+                          </label>
+                          <p
+                            style="margin-left: 30px !important"
+                            v-html="field.goal"
+                          ></p>
                         </div>
                       </div>
                     </div>
                   </div>
-                </form>
-                <div class="row" v-else>
-                  <div class="col-12">
-                    <div class="alert alert-warning">
-                      Employee yet to submit assessment
-                    </div>
+                </div>
+              </form>
+              <div class="row" v-else>
+                <div class="col-12">
+                  <div class="alert alert-warning">
+                    Employee yet to submit assessment
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="p-3 bg-light mb-4">
-                  <h5 class="font-size-14 mb-0">Employee Details</h5>
-                </div>
-                <div class="d-flex justify-content-between text-capitalize">
-                  <p>Employee Name</p>
-                  <p>
-                    {{ currentEmployee.emp_first_name }}
-                    {{ currentEmployee.emp_last_name }}
-                  </p>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <p>T7 Number</p>
-                  <p>{{ currentEmployee.emp_unique_id }}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <p>Phone Number</p>
-                  <p>{{ currentEmployee.emp_phone_no }}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <p>Office Email</p>
-                  <p>{{ currentEmployee.emp_office_email }}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <p>T3 Code</p>
-                  <p>{{ currentEmployee.sector.d_t3_code }}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                  <p class="mb-0">T6 Code</p>
-                  <p class="mb-0">{{ currentEmployee.location.l_t6_code }}</p>
-                </div>
-              </div>
-            </div>
-
-            <div class="card" v-if="authuser === currentEmployee.emp_supervisor_id">
-              <div class="card-body">
-                <form @submit.prevent="processAssessment">
-                  <div class="btn-group d-flex ">
-                    <button class="btn btn-success btn-sm" :disabled="assessStatus === 1 ? true : false"> <i class="mdi mdi-check mr-2"></i> Approve </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-
           </div>
         </div>
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <div class="p-3 bg-light mb-4">
+                <h5 class="font-size-14 mb-0">Employee Details</h5>
+              </div>
+              <div class="d-flex justify-content-between text-capitalize">
+                <p>Employee Name</p>
+                <p>
+                  {{ currentEmployee.emp_first_name }}
+                  {{ currentEmployee.emp_last_name }}
+                </p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p>T7 Number</p>
+                <p>{{ currentEmployee.emp_unique_id }}</p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p>Phone Number</p>
+                <p>{{ currentEmployee.emp_phone_no }}</p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p>Office Email</p>
+                <p>{{ currentEmployee.emp_office_email }}</p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p>T3 Code</p>
+                <p>{{ currentEmployee.sector.d_t3_code }}</p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p class="mb-0">T6 Code</p>
+                <p class="mb-0">{{ currentEmployee.location.l_t6_code }}</p>
+              </div>
+            </div>
+          </div>
 
+          <div
+            class="card"
+            v-if="authuser === currentEmployee.emp_supervisor_id"
+          >
+            <div class="card-body">
+              <form @submit.prevent="processAssessment">
+                <div class="btn-group d-flex">
+                  <button
+                    class="btn btn-success btn-sm"
+                    :disabled="assessStatus === 1 ? true : false"
+                  >
+                    <i class="mdi mdi-check mr-2"></i> Approve
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
